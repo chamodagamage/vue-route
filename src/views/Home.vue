4bc209cb28d22c4fbcf6116ab8ef8f26e7756ebe
chamoda gamage <karunarathna91@gmail.com>
2021-01-12 11:18:38 +1100

    add routers

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <h1>Home</h1>
+   <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores error saepe doloremque in harum excepturi hic labore reiciendis adipisci aspernatur, magni commodi non sit fugiat dolore eaque qui nesciunt modi necessitatibus fugit officiis. Modi eius vero beatae amet, unde tempore quibusdam et quidem repellendus. Perferendis debitis magni nemo nobis eveniet quod aliquid vel temporibus dolorum incidunt consequuntur esse earum, reprehenderit ipsa commodi voluptas harum id nostrum voluptatem dignissimos obcaecati recusandae? Dicta dolore officia sed necessitatibus doloribus! Maiores sit assumenda velit, nesciunt dolores nisi officia doloribus fugiat, quod dolorem, quas nemo. Minima impedit voluptatum officia blanditiis vitae odio autem sapiente architecto!</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores error saepe doloremque in harum excepturi hic labore reiciendis adipisci aspernatur, magni commodi non sit fugiat dolore eaque qui nesciunt modi necessitatibus fugit officiis. Modi eius vero beatae amet, unde tempore quibusdam et quidem repellendus. Perferendis debitis magni nemo nobis eveniet quod aliquid vel temporibus dolorum incidunt consequuntur esse earum, reprehenderit ipsa commodi voluptas harum id nostrum voluptatem dignissimos obcaecati recusandae? Dicta dolore officia sed necessitatibus doloribus! Maiores sit assumenda velit, nesciunt dolores nisi officia doloribus fugiat, quod dolorem, quas nemo. Minima impedit voluptatum officia blanditiis vitae odio autem sapiente architecto!</p>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+<script>
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+
   }
 }
 </script>
